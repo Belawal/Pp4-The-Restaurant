6bc4e1fae377ef101ca3f325dcca6300141a102d
Belawal khan <Belawal.khan@gmail.com>
2025-02-20 13:23:44 +0000

    connected homepage to booking app

--- restaurant_project/urls.py
+++ restaurant_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('booking/', include('booking.urls')),  # Connects to the Booking app
+    path('booking/', include('booking.urls')),  # Booking app
+    path('', include('booking.urls')),  # Homepage
 ]
