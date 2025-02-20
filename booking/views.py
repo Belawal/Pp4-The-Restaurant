4b8e6cafa8ad5727c5563a956a252e986ae41796
Belawal khan <Belawal.khan@gmail.com>
2025-02-20 13:19:49 +0000

    added homepage to show welcome message

--- booking/views.py
+++ booking/views.py
@@ -1,4 +1,4 @@
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("Hello! This is the Booking app.")
+    return HttpResponse("<h1>Hello! This is the Booking app.</h1><p>Book a your table</p>")
