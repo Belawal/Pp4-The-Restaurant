440054d83c540e91d6ff7bc771dffa9dcbf50d9e
Belawal khan <Belawal.khan@gmail.com>
2025-02-21 08:53:25 +0000

    updated css to fix navbar and bacground image  issues

--- restaurant_project/settings.py
+++ restaurant_project/settings.py
@@ -116,7 +116,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/' # URL prefix for static files
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static"  # Tells Django where to find static files
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
