295e7318424e20ab47b1b358a4bffdf19f08dc88
Belawal khan <Belawal.khan@gmail.com>
2025-02-22 13:14:01 +0000

    add logout url path

--- restaurant_project/booking/urls.py
+++ restaurant_project/booking/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.home, name="home"),  # Route for homepage
     path("signup/", views.signup, name="signup"),  # Route for user signup
     path("login/", views.login_view, name="login"),  # Route for user login
+    path('logout/', views.logout_view, name='logout'),  # Logout page
 ]
